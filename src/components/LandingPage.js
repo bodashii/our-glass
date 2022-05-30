12367ffc5e66e9add82e8c9f8b7aaf1c7965fa27
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-29 23:20:35 -0500

    added html function to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,6 +5,17 @@ import '../../App.css'
 
 export default function LandingPage() {
     return (
-        <div></div>
+        <header>
+            <h1 className="main-title text-center">login / register page</h1>
+            <p className="main-para text-center">join us now and don't waste time</p>
+            <div className="buttons text-center">
+                <Link to="/login">
+                    <button className="primary-button">log in</button>
+                </Link>
+                <Link to="/register">
+                    <button className="primary-button" id="reg_btn"><span>register </span></button>
+                </Link>
+            </div>
+        </header>
     )
 }
