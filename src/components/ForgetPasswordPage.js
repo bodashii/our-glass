4bc2b5a0d3adb706b83b7ea792780221d170ec1f
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-30 16:46:57 -0500

    translated forgotpasswordpage into expo elements

--- src/components/ForgetPasswordPage.js
+++ src/components/ForgetPasswordPage.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
+import { ButtonGroup, Text } from 'react-native-elements'
 
 
 
@@ -16,15 +17,33 @@ export default function ForgetPasswordPage() {
     <View style={styles.container}>
       <Text>Reset your password</Text>
       <Text>Enter your email address and we will send you a new password</Text>
-          <TextInput name="fpassword" placeholder="password" />
-          
+      <TextInput name="fpassword" placeholder="password" />
+      <TextInput name="email" placeholder="email" />
+      <TouchableOpacity
+        style={styles.submitButton}
+      //  onPress = {
+      >
+        <Text style={styles.submitButtonText}> Submit </Text>
+      </TouchableOpacity>
+      <View style={styles.container}>
+        <Text>New to the site?</Text>
+        <Pressable
+        title='Create an account'
+        onPress={() => navigation.navigate('RegisterPage')}
+        />
+        <Pressable
+        title='Home Page'
+        onPress={() => navigation.navigate('HomePage')}
+        />
+      </View>
     </View>
+
     // <div className="text-center m-5-auto">
     //     <h2>Reset your password</h2>
     //     <h5>Enter your email address and we will send you a new password</h5>
     //     <form action="/login">
     //         <p>
-    //             <label id="reset_pass_lbl">Email address</label><br/>
+    //             <label id="reset_pass_lbl">Email address</label><br />
     //             <input type="email" name="email" required />
     //         </p>
     //         <p>
@@ -36,15 +55,15 @@ export default function ForgetPasswordPage() {
     //         <p><Link to="/">Back to Homepage</Link>.</p>
     //     </footer>
     // </div>
-  );
+      );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+      const styles = StyleSheet.create({
+        container: {
+        flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
   },
 });
 
