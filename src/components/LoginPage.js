57171f47ad753753a6e62bd389fcf89269f07c03
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-29 23:49:03 -0500

    created login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,6 +5,27 @@ import '../../App.css'
 
 export default function SignInPage() {
     return (
-        <div></div>
+        <div className="text-center m-5-auto">
+            <h2>Sign in to Our-Glass</h2>
+            <form action="/home">
+                <p>
+                    <label>Username or email address</label><br/>
+                    <input type="text" name="first_name" required />
+                </p>
+                <p>
+                    <label>Password</label>
+                    <Link to="/forget-password"><label className="right-label">Forgot password?</label></Link>
+                    <br/>
+                    <input type="password" name="password" required />
+                </p>
+                <p>
+                    <button id="sub_btn" type="submit">Login</button>
+                </p>
+            </form>
+            <footer>
+                <p>New to the site? <Link to="/register">Create an account</Link>.</p>
+                <p><Link to="/">Back to Homepage</Link>.</p>
+            </footer>
+        </div>
     )
 }
