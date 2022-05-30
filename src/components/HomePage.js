c9051810117e60748061066cbec49d07773e16a7
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-29 23:29:35 -0500

    added content to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function HomePage() {
     return (
-        <div></div>
+        <div className="text-center">
+        <h1 className="main-title home-page-title">Welcome to Our-Glass!</h1>
+        <Link to="/">
+            <button className="primary-button">Log out</button>
+        </Link>
+        </div>
     )
 }
