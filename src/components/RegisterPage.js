edec965d7a09e21a432331186035f6cf320ec75b
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-29 23:07:22 -0500

    skeleton for registerpage

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../../App.css'
+
+export default function SignUpPage() {
+
+    return (
+        <div></div>
+    )
+}
