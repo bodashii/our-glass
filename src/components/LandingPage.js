1be00b92f819f1e2906135fe510a7df68ee31c79
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-29 23:26:14 -0500

    added styling and bg image to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import '../../App.css'
+import BackgroundImage from '../../assets//bglanding.png'
 
 export default function LandingPage() {
     return (
-        <header>
+        <header style={ HeaderStyle }>
             <h1 className="main-title text-center">login / register page</h1>
             <p className="main-para text-center">join us now and don't waste time</p>
             <div className="buttons text-center">
@@ -18,4 +19,13 @@ export default function LandingPage() {
             </div>
         </header>
     )
+}
+
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
 }
