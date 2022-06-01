ec7fa46b48583af683c481d3057ca7792b544387
Mark Estrada <h.mark.estrada@gmail.com>
2022-06-01 10:58:49 -0500

    GraphQL Testing
    Updated app.js and shema.js for testing

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const graphqlHTTP = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema.js')
+
 const app = express()
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: schema,
-  })
-)
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}))
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('App listening on port 3000')
 })
+
