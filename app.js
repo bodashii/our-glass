f8730128e2620384d1341549cc0c8676f13a430b
Mark Estrada <h.mark.estrada@gmail.com>
2022-06-01 08:33:31 -0500

    MongoDB Testing Branch
    Created the branch to resolve push and pull from Mongo Database.

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://ourglass:test1234@cluster0.wkxpvy0.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("our-glass-test");
+  var myobj = [
+    { name: 'Derek', address: 'Highway 71'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Amy', address: 'Apple st 652'},
+    { name: 'Hannah', address: 'Mountain 21'},
+    { name: 'Michael', address: 'Valley 345'},
+    { name: 'Sandy', address: 'Ocean blvd 2'},
+    { name: 'Betty', address: 'Green Grass 1'},
+    { name: 'Richard', address: 'Sky st 331'},
+    { name: 'Susan', address: 'One way 98'},
+    { name: 'Vicky', address: 'Yellow Garden 2'},
+    { name: 'Ben', address: 'Park Lane 38'},
+    { name: 'William', address: 'Central st 954'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'Viola', address: 'Sideway 1633'}
+  ];
+  dbo.collection("User").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
