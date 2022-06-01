7fef4c3f95163dc227068cc855dc420af36b119e
Mark Estrada <h.mark.estrada@gmail.com>
2022-06-01 10:44:57 -0500

    GraphQL Files
    Testing and debugging GraphQL

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
-var express = require("express");
-var app = express();
-var port = 3000;
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema.js')
+const app = express()
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+  })
+)
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://ourglass:test1234@cluster0.wkxpvy0.mongodb.net/?retryWrites=true&w=majority";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("our-glass-test");
-  var myobj = [
-    { name: 'Derek', address: 'Highway 71'},
-    { name: 'Chuck', address: 'Main Road 989'},
-    { name: 'John', address: 'Highway 71'},
-    { name: 'Peter', address: 'Lowstreet 4'},
-    { name: 'Amy', address: 'Apple st 652'},
-    { name: 'Hannah', address: 'Mountain 21'},
-    { name: 'Ben', address: 'Park Lane 38'},
-    { name: 'William', address: 'Central st 954'},
-    { name: 'Chuck', address: 'Main Road 989'},
-    { name: 'Viola', address: 'Sideway 1633'}
-  ];
-  dbo.collection("User").insertMany(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("Number of documents inserted: " + res.insertedCount);
-    db.close();
-  });
-});
+app.listen(3000, () => {
+  console.log('App listening on port 3000')
+})
